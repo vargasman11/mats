d57785af044892571e92d577c50b684e2ce76ec8
Jerry Vargas <jvargas@conversionia.com>
2020-03-25 11:16:41 -0500

    changes

---
+++ web/scripts/custom.js
@@ -0,0 +1,9 @@
+var apply = bodymovin.loadAnimation({
+    container: document.getElementById('applyLottie'), // Required
+    path: '/render_apply/data.json', // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+  })
+
+
