b9c2428a2d3615a7fec054b3475f3d5723b49f7a
Jerry Vargas <jvargas@conversionia.com>
2020-03-25 19:55:19 -0500

    adding lottie

--- web/scripts/custom.js
+++ web/scripts/custom.js
@@ -1,6 +1,24 @@
 var apply = bodymovin.loadAnimation({
-    container: document.getElementById('applyLottie'), // Required
+    container: document.getElementById('lottieApply'), // Required
     path: '/render_apply/data.json', // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+  })
+
+
+  var chat = bodymovin.loadAnimation({
+    container: document.getElementById('lottieChat'), // Required
+    path: '/render_chat/data.json', // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+  })
+
+  // Mobile Version
+  var chatM = bodymovin.loadAnimation({
+    container: document.getElementById('lottieChatM'), // Required
+    path: '/render_chat/data.json', // Required
     renderer: 'svg', // Required
     loop: true, // Optional
     autoplay: true, // Optional
